bb7920300478bac0fc1bafa3258152d84fc248f2
Mandefro Endalk <mandefroendalk00@gmail.com>
2024-12-08 03:49:48 -0800

    last-part

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Book = require("../database/Book");
+const User = require("../database/User");
 const { authPage } = require("../middleware/middleware");
 
 router.get("/books/all", authPage(["Admin"]), async (req, res) => {
@@ -12,7 +13,23 @@ router.get("/books/all", authPage(["Admin"]), async (req, res) => {
 });
 router.get("/books/", async (req, res) => {
   try {
-    const books = await Book.find();
+    const { userId } = req.user;
+    const user = await User.findById(userId);
+    const favioriteBooks = user?.favourites || [];
+    const preferedGeneres = user?.preferences?.genres || [];
+
+    let books;
+    if (favioriteBooks.length > 0) {
+      books = await Book.find({ _id: { $in: favioriteBooks } });
+    } else if (preferedGeneres.length > 0) {
+      books = await Book.find({ genre: { $in: preferedGeneres } });
+    } else {
+      books = await Book.find();
+    }
+    if (!books || books.length === 0) {
+      return res.status(404).json("No books available");
+    }
+
     res.status(200).json(books);
   } catch (error) {
     res.status(404).send("Error in getting books from database", error.message);
@@ -20,12 +37,18 @@ router.get("/books/", async (req, res) => {
 });
 
 router.post("/books", authPage(["Student"]), async (req, res) => {
-  const { title, author, isbn, publishedYear } = req.body;
+  const { title, author, isbn, publishedYear, genre } = req.body;
   try {
     if (!title || !author || !isbn || !publishedYear)
       return res.status(400).json("All fields are required");
 
-    const newBook = new Book({ title, author, ISBN: isbn, publishedYear });
+    const newBook = new Book({
+      title,
+      author,
+      ISBN: isbn,
+      publishedYear,
+      genre,
+    });
     await newBook.save();
     res.status(201).json({ message: "Book added succesfuly;" });
   } catch (error) {}
@@ -34,7 +57,7 @@ router.post("/books", authPage(["Student"]), async (req, res) => {
 router.put("/books/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const { title, author, isbn, publishedYear } = req.body;
+    const { title, author, isbn, publishedYear, genre } = req.body;
 
     if (!title || !author || !isbn || !publishedYear)
       return res.status(400).json("All fields are required");
@@ -45,6 +68,7 @@ router.put("/books/:id", async (req, res) => {
         author,
         ISBN: isbn,
         publishedYear,
+        genre,
       },
       { new: true }
     );
